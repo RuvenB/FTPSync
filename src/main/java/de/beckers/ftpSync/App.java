f507c6323cb6358be674b0d6219134d699a1114d
Ruven Beckers <derfremde@hotmail.com>
2020-12-19 14:29:42 +0100

    Maven archetype erstellt

---
+++ src/main/java/de/beckers/ftpSync/App.java
@@ -0,0 +1,13 @@
+package de.beckers.ftpSync;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
